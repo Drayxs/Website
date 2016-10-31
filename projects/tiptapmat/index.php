7e8feb413247d2e75c48d955bb8108c6d6816792
Gary Ng <ng.gary.kc@gmail.com>
2016-11-01 00:11:58 +0900

    TTM Project Page Updates
    - Cropped Logo
    - Reformatted
    - New sitemap

--- projects/tiptapmat/index.php
+++ projects/tiptapmat/index.php
@@ -22,77 +22,52 @@
 <?php include "../../includes.php"; ?>	
 
 <div>
-	<img src = "img/tiptapmat_logo.png" style = "width: 100%;" title = "Tip Tap Mat: Unlock locked doors with your feet" alt = "Tip Tap Mat" />
-	
-	<br>
-	
-	<table>
-		<tr>
-			<td class = "projectDescriptionSide">
-				<p style = "text-align: left;">					
-					Normally, you unlock doors with a key or enter a keycode on a key pad. That works fine most of the time, but what if you just went shopping and were carrying a lot of bags? Everyone, no matter who you are or where you are from, knows that feeling of dread when your hands aren't free and you see a locked door.<br><br>
-					
-					<i>Tip Tap Mat</i> is a smart door mat which allows you to unlock locked doors with your feet. By using your feet to tap the <b>correct combination</b> on the mat, a wireless signal is sent to the unlocker which unlocks a digital lock.
-				</p>
-			</td>
-			<td class = "projectVideoSide">
-				<div class = "youtube-container">
-					<div class = "youtube-player" data-id = "sS6sqPPw__o"></div>					
-				</div>
-			</td>		
-		</tr>
-		<tr>
-			<td class = "projectDescriptionSide">
-				<p style = "text-align: left;">					
-					Since most things nowadays are designed for your hands, your hands are often very busy. Your feet on the other hand are generally fairly free. We wanted to explore a <b>different type of interaction</b> and designed an unlocking interface that moves the interaction from your hands to your feet.<br><br>
+	<img src = "img/tiptapmat_logo_cropped.png" style = "width: 100%;" title = "Tip Tap Mat: Unlock locked doors with your feet" alt = "Tip Tap Mat" />
+	<p style = "font-size: 10px">Designed by: <a href = "http://www.garykcng.com/">Gary Ng</a>, Punyotai Thamjamrassri, Giulia Scurati</p>
 
-					Pressure sensors under the mat detect your movements then transmit the data to the door's lock. When the correct combination is entered, the door is unlocked. <i>Tip Tap Mat</i> can be operated by everyone and is ideal in situations where the can unlock the door without having to put down whatever they're holding, making it a <b>hassle free experience</b>.
-				</p>
-			</td>
-			<td class = "projectVideoSide">
-				<div class = "projectPictures">
-					<a href = "img/tiptapmat_mat_full.jpg" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_mat.jpg" title = "A mat with a console installed" /></a>
-					<a href = "img/tiptapmat_door_full.jpg" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_door.jpg" title = "The demo setup with a small door" /></a>					
-				</div>
-				<br>
-			</td>
-		</tr>
-		
-		<tr>
-			<td class = "projectDescriptionSide">
-				<p style = "text-align: left;">
-						<b>Designers</b>
-				</p>
-				<ul>
-					<li><a href = "http://www.garykcng.com/">Gary Ng</a></li>
-					<li>Punyotai Thamjamrassri</li>
-					<li>Giulia Scurati</li>
-				</ul>
-				
-				<p style = "text-align: left;">
-					<b>External Links</b>
-				</p>
-				<ul>
-					<li><a href = "http://www.dubaidesignweek.ae/global-grad-show/projects/tiptapmat/" target = "_blank">Dubai Design Week</a></li>
-					<li><a href = "http://www.globalgradshow.com/projects/tip-tap-mat/" target = "_blank">Global Grad Show</a></li>
-				</ul>
+	<p style = "text-align: left;">					
+		Normally, you unlock doors with a key or enter a key code on a key pad. That works fine most of the time, but what if you just went shopping and were carrying a lot of bags? Everyone, no matter who you are or where you are from, knows that feeling of dread when your hands aren't free and you see a locked door.<br><br>
 					
-				<p style = "text-align: left;">
-					<b>Demos</b>
-				</p>
-				<ul>
-					<li><a href = "http://www.globalgradshow.com/" target = "_blank">Global Grad Show</a>, <a href = "http://www.dubaidesignweek.ae/" target = "_blank">Dubai Design Week 2016</a>. October 24, 2016 - October 29, 2016. Dubai, United Arab Emirates.</li>
-				</ul>
-			</td>
-			<td class = "projectVideoSide">
-				<div class = "projectPictures">
-					<a href = "img/tiptapmat_howitworks_full.png" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_howitworks.jpg" title = "How it works" /></a>
-					<a href = "img/tiptapmat_howtouse_full.png" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_howtouse.jpg" title = "How to use it" /></a>					
-				</div>
-				<br>
-			</td>
-		</tr>
-	</table>
+		<i>Tip Tap Mat</i> is a smart door mat which allows you to <b>unlock locked doors with your feet</b>. By using your feet to tap the <b>correct combination</b> on the mat, a wireless signal is sent to the unlocker which unlocks a digital lock.
+	</p>
+	
+	<div class = "youtube-container" style = "max-width: 50%;">
+		<div class = "youtube-player" data-id = "sS6sqPPw__o"></div>					
+	</div>
+	
+	<p style = "text-align: left;">					
+		Since most things nowadays are designed for your hands, they are often very busy. Your feet on the other hand are generally fairly free. We wanted to explore a <b>different type of interaction</b> and designed an unlocking interface that moves the interaction from your hands to your feet.<br><br>
+
+		Pressure sensors under the mat detect your movements then transmit the data to the door's lock. When the correct combination is entered, the door is unlocked. <i>Tip Tap Mat</i> can be operated by everyone and is ideal in situations where the can unlock the door without having to put down whatever they're holding, making it a <b>hassle free experience</b>.<br><br>
+		
+		<i>Tip Tap Mat</i> is easily installed. The mat console is attached to the corner of any rectangular mat and the sensors are placed underneath. Then, one of two unlocking systems are used. The first uses an unlocker module that is mounted onto an existing door lock. The second uses a custom lock which has the wireless controller built into it.
+	</p>
+	
+	<div class = "projectPictures">
+		<a href = "img/tiptapmat_mat_full.jpg" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_mat.jpg" title = "A mat with a console installed" /></a>
+		<a href = "img/tiptapmat_door_full.jpg" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_door.jpg" title = "A demo setup with a small door" /></a>
+	</div>
+	
+	<div class = "projectPictures">
+		<a href = "img/tiptapmat_howitworks_full.png" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_howitworks.jpg" title = "How it works" /></a>		
+		<a href = "img/tiptapmat_howtouse_full.png" target = "_blank"><img style = "width: 50%;" src = "img/tiptapmat_howtouse.jpg" title = "How to use it" /></a>	
+	</div>
+	
+	<hr>
+	
+	<p style = "text-align: left;"><b>External Links</b></p>
+	
+	<ul>
+		<li><a href = "http://www.dubaidesignweek.ae/global-grad-show/projects/tiptapmat/" target = "_blank">Dubai Design Week</a></li>
+		<li><a href = "http://www.globalgradshow.com/projects/tip-tap-mat/" target = "_blank">Global Grad Show</a></li>
+	</ul>
+		
+	<p style = "text-align: left;"><b>Demos</b></p>
+	
+	<ul>
+		<li><a href = "http://www.globalgradshow.com/" target = "_blank">Global Grad Show</a>, <a href = "http://www.dubaidesignweek.ae/" target = "_blank">Dubai Design Week 2016</a>. October 24, 2016 - October 29, 2016. Dubai, United Arab Emirates.</li>
+	</ul>
+	
 </div>
 
 <?php include "../../footer.php"; ?>
